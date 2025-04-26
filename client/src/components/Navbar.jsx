8a791a86969a6460855e521f728ea7c9a4c52b3f
Adit122022 <adity202212@gmail.com>
2025-04-27 03:13:08 +0530

    feat: Refactor Hero, Navbar, and Register components for improved layout and animations

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="bg-white overflow-hidden dark:bg-gray-900  border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white overflow-hidden dark:bg-gray-900  fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center      justify-between mx-auto p-4">
         {/* Logo */}
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -73,8 +73,8 @@ export default function Navbar() {
           id="navbar-sticky"
         >
           <ul className="flex flex-col  p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg 
-                         bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 
-                         md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                          md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 
+                            bg-transparent">
             <li className="relative">
              
              <GooeyNav
